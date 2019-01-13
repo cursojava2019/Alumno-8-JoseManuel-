c49ec0c9b353d7001dbe2d69b286cbac0c617af7
Jose <Jose@Molino-portatil>
2019-01-13 20:16:17 +0100

    Ejercicio 03 terminado

---
+++ EjerciciosObjetos1/src/es/indra/ejercicio3/Ejercicio3Main.java
@@ -0,0 +1,34 @@
+package es.indra.ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3Main {
+
+	public static void main(String[] args) {
+		Password[] contrasenas;
+		boolean[] esFuerte;
+		Scanner entrada= new Scanner(System.in);
+		int nArray=0;
+		System.out.println("Introduce el numero de contrasenas a generar");
+		nArray=entrada.nextInt();
+		entrada.nextLine();
+		contrasenas = new Password[nArray];
+		esFuerte = new boolean[nArray];
+		for(int i=0;i<nArray;i++) {
+			System.out.println("Que longitud quieres que tenga tu password?");
+			Integer longitud= new Integer(entrada.nextInt());
+			contrasenas[i]= new Password(longitud);
+			esFuerte[i]=contrasenas[i].esFuerte(contrasenas[i].getPass());
+			if(esFuerte[i]) {
+				System.out.println(contrasenas[i].getPass()+" es fuerte");
+				System.out.println(esFuerte[i]);
+			}else {
+				System.out.println(contrasenas[i].getPass()+" es debil");
+				System.out.println(esFuerte[i]);
+			}
+			
+		};
+			
+	}
+
+}
---
+++ EjerciciosObjetos1/src/es/indra/ejercicio3/Password.java
@@ -0,0 +1,77 @@
+package es.indra.ejercicio3;
+
+public class Password {
+	protected Integer longitud;
+	protected String pass;
+	public Password(Integer longitud, String pass) {
+		super();
+		this.longitud = new Integer(8);
+		this.pass = pass;
+	}
+	public Password(Integer longitud) {
+		super();
+		this.pass=generarPass(longitud);
+	}
+	public Integer getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(Integer longitud) {
+		this.longitud = longitud;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public String generarPass(Integer longitud) {
+		String pass="",numPass="";
+		String lMayus="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		String lMinus="abcdefghijklmnñopqrstuvwxyz";
+		numPass=devolverNum(longitud);
+		pass+= Character.toString(devolverLetra(lMayus));
+		pass+= Character.toString(devolverLetra(lMayus));
+		pass+= numPass;
+		pass+= Character.toString(devolverLetra(lMinus));
+		return pass;
+	}
+	protected String devolverNum(Integer longitud) {
+		String numPass = "";
+		int num=0;
+		for(int i=0;i<longitud;i++) {
+			num=Integer.valueOf((int) Math.floor((Math.random()*longitud)+1));
+			numPass += Integer.toString(num);
+		}
+		return numPass;
+	}
+	protected char devolverLetra(String letras) {
+		char letra;
+		letra = letras.charAt((int) (Math.random() * 23) + 1);
+		return letra;
+	}
+
+	public boolean esFuerte(String pass) {
+		boolean esFuerte=false;
+		char[] caracteres = pass.toCharArray();
+		int contMayus=0,conMinus=0,conNum=0;
+		 for(int i=0; i<pass.length();i++) { 
+		      if(Character.isUpperCase(caracteres[i])){
+		    	  contMayus++;
+		    	  System.out.println("Hola1");
+		      }else if(Character.isLowerCase(caracteres[i])){
+		    	  conMinus++;
+		    	  System.out.println("Hola2");
+		      }else if(Character.isDigit(caracteres[i])) {
+		    	  conNum++;
+		    	  
+		      }
+		}
+		if(conMinus>=1 && contMayus>=2 && conNum>=5) {
+			esFuerte=true;
+		}else {
+			System.out.println("conMayus "+contMayus);
+			System.out.println("conMinus "+conMinus);
+			System.out.println("conNum "+conNum);
+			esFuerte=false;
+		}
+		return esFuerte;
+	}
+	
+}
