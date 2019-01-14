7925576504c79df7cfc3a306972646ee630346e0
CURSOJAVA <CURSOJAVA@192.168.1.73>
2019-01-14 17:39:09 +0100

    Ejer 08 Implementacion del Main

--- EjerciciosObjetos1/src/es/indra/ejercicio8/Ejercicio8Main.java
+++ EjerciciosObjetos1/src/es/indra/ejercicio8/Ejercicio8Main.java
@@ -2,10 +2,29 @@ package es.indra.ejercicio8;
 
 public class Ejercicio8Main {
 
-	public static void main(String[] args) {
-		//Estudiantes e1= new Estudiantes();
-		
+	public int totalAlumnos(boolean ausente) {
+		int totalAlumnos = 0;
+		if (ausente == false)
+			totalAlumnos++;
+		return totalAlumnos;
+	}
 
+	public static void main(String[] args) {
+
+		Estudiantes[] estudiantes = new Estudiantes[5];
+		Profesosr p1 = new Profesosr();
+		int totalAlumnos = 0;
+		for (int i = 0; i < estudiantes.length; i++) {
+			estudiantes[i] = new Estudiantes();
+		}
+		System.out.println(p1.toString());
+		for (int i = 0; i < estudiantes.length; i++) {
+			System.out.println(estudiantes[i].toString());
+		}
+
+		for (int i = 0; i < estudiantes.length; i++) {
+			totalAlumnos = totalAlumnos(estudiantes[i].ausente());
+		}
 	}
 
 }
