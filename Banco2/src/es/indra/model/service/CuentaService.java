a8aea806b1d9b6981adcb16ea57271407b957136
Jose <Jose@Molino-portatil>
2019-01-22 11:12:26 +0100

    Banco2 Guardado correctamente en BBDD de cliente y cuenta.

---
+++ Banco2/src/es/indra/model/service/CuentaService.java
@@ -0,0 +1,33 @@
+package es.indra.model.service;
+
+import java.util.List;
+
+import es.indra.model.dao.CuentaDao;
+import es.indra.model.entities.Cuenta;
+import es.indra.model.support.Dao;
+import es.indra.model.support.DaoException;
+import es.indra.model.support.Service;
+
+public class CuentaService extends Service<Long, Cuenta> {
+	private CuentaDao dao = null;
+
+	@Override
+	protected Dao<Long, Cuenta> getDao() {
+		if (this.dao == null) {
+			this.dao = new CuentaDao();
+		}
+		return this.dao;
+	}
+
+//	public List<Cuenta> findCuentasBloqueadas() {
+//		try {
+//			getDao();
+//			return this.dao.findCuentaBloqueadas();
+//		} catch (DaoException e) {
+//			System.out.println("Error Al obtener informacion en la BBDD ");
+//			return null;
+//		}
+//
+//	}
+
+}
--- Banco2/src/es/indra/model/support/DatosConexion.java
+++ Banco2/src/es/indra/model/support/DatosConexion.java
@@ -2,8 +2,8 @@ package es.indra.model.support;
 
 public class DatosConexion {
 
-	public static final String URL = "jdbc:postgresql://127.0.0.1/Banco2";
+	public static final String URL = "jdbc:postgresql://localhost/Banco2";
 	public static final String USUARIO_BD = "postgres";
-	public static final String PASSWORD_BD = "123";
+	public static final String PASSWORD_BD = "";
 
 }
